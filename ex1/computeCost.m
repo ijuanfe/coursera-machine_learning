0f5a3f4064c6ac28951c2fddd06ad617835dbab1
ijuanfe <juan.orozco.escobar@correounivalle.edu.co>
2020-11-28 01:56:12 -0500

    Add warmUpExercise.m, plotData.m, computeCost.m, computeCostMulti.m, gradientDescent.m solutions

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -6,16 +6,21 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% Solution using loops:
+% - Only works for two theta parameters and two features x (atributes)
+% - Check computeCostMulti.m implementation for any number of theta parameters and features
+for i = 1:m
+  J += power(((theta(1) * X(i, 1) + (theta(2) * X(i, 2))) - y(i)), 2);
+end
 
-
-
+J = 1/(2*m) * J;
 
 % =========================================================================
 
--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -6,16 +6,37 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% 1. Solution using loops:
+JAux = 0;
+t = length(theta);
 
+for i = 1:m
+  for j = 1:t
+    JAux += theta(j) * X(i, j);
+  end
+  J += power(JAux - y(i), 2);
+  JAux = 0;
+end
 
+J = 1/(2*m) * J;
 
+% 2. Solution using vectorization:
+% - Vectorization is the process of converting an algorithm from operating
+%   on a single value at a time to operating on a set of values at one time
+%J = sum(((theta' * X')' - y).^2) / (2 * m)
+
+% Alternative to 2nd solution:
+% - Recall some matrix properties
+%  - Conversion 1. (AB)' = B'A' --> theta' * X'   = (X * theta)'
+%  - Conversion 2. (A')' = A    --> (X * theta)'' = X * theta
+%J = sum(((X * theta) - y).^2) / (2 * m)
 
 % =========================================================================
 
